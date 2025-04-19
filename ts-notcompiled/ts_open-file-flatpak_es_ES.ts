e9ac6ff5af594f78feea914b13f795c08d580612
krafairus <64279814+krafairus@users.noreply.github.com>
2025-04-19 12:15:55 -0500

    Add files via upload

---
+++ ts-notcompiled/ts_open-file-flatpak_es_ES.ts
@@ -0,0 +1,145 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>FlatpakInstaller</name>
+    <message>
+        <location filename="open-file-flatpak" line="107"/>
+        <source>Instalador de Flatpak</source>
+        <translation>Instalador de Flatpak</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="118"/>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="118"/>
+        <source>No se seleccionó ningún archivo.</source>
+        <translation>No se seleccionó ningún archivo.</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="139"/>
+        <source>¡Advertencia! Este paquete ya está instalado.</source>
+        <translation>¡Advertencia! Este paquete ya está instalado.</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="145"/>
+        <source>Nombre</source>
+        <translation>Nombre</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="146"/>
+        <source>Título</source>
+        <translation>Título</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="147"/>
+        <source>Rama</source>
+        <translation>Rama</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="148"/>
+        <source>URL</source>
+        <translation>URL</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="149"/>
+        <source>Nombre remoto sugerido</source>
+        <translation>Nombre remoto sugerido</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="150"/>
+        <source>Es un runtime</source>
+        <translation>Es un runtime</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="151"/>
+        <source>Repositorio del runtime</source>
+        <translation>Repositorio del runtime</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="320"/>
+        <source>Clave GPG</source>
+        <translation>Clave GPG</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="195"/>
+        <source>Cancelar</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="214"/>
+        <source>Desinstalar</source>
+        <translation>Desinstalar</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="231"/>
+        <source>Instalar</source>
+        <translation>Instalar</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="267"/>
+        <source>No se pudo cargar el archivo de configuración. Usando idioma predeterminado.</source>
+        <translation>No se pudo cargar el archivo de configuración. Usando idioma predeterminado.</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="277"/>
+        <source>Traducción cargada para el idioma: {language}</source>
+        <translation>Traducción cargada para el idioma: {language}</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="279"/>
+        <source>No se pudo cargar la traducción para el idioma: {language} en la ruta: {translation_path}</source>
+        <translation>No se pudo cargar la traducción para el idioma: {language} en la ruta: {translation_path}</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="283"/>
+        <source>Seleccionar archivo .flatpakref</source>
+        <translation>Seleccionar archivo .flatpakref</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="284"/>
+        <source>Archivos Flatpak (*.flatpakref)</source>
+        <translation>Archivos Flatpak (*.flatpakref)</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="320"/>
+        <source>No se encontró una clave GPG en el archivo.</source>
+        <translation>No se encontró una clave GPG en el archivo.</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="383"/>
+        <source>Finalizar</source>
+        <translation>Finalizar</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="404"/>
+        <source>Operación cancelada</source>
+        <translation>Operación cancelada</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="405"/>
+        <source>La operación ha sido cancelada.</source>
+        <translation>La operación ha sido cancelada.</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="408"/>
+        <source>Cerrar</source>
+        <translation>Cerrar</translation>
+    </message>
+</context>
+<context>
+    <name>GPGKeyDialog</name>
+    <message>
+        <location filename="open-file-flatpak" line="18"/>
+        <source>Clave GPG</source>
+        <translation>Clave GPG</translation>
+    </message>
+    <message>
+        <location filename="open-file-flatpak" line="34"/>
+        <source>Cerrar</source>
+        <translation>Cerrar</translation>
+    </message>
+</context>
+</TS>
